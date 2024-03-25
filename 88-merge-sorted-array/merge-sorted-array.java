b00cf634295336d903d5dac81befbf1424c7611c
suriya216 <136613620+suriya216@users.noreply.github.com>
2024-03-25 21:07:18 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (41.32%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,10 +1,18 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i=m;
-        for(int j=0;j<n;j++){
-            nums1[i]=nums2[j];
-            i++;
-        }
-        Arrays.sort(nums1);
+        int l1=m-1; //Length of nums1 including space for nums2 to add up
+        int l2=n-1; //Length of nums2
+        int len=m+n-1; //Total length of two arrays(nums1 & nums2)        
+        while (l2>=0){ 
+            //For iterating to the length of nums2 because it should be added to array (nums1)
+            //Iterate in reverse way
+            if(l1>=0&&nums1[l1]>nums2[l2]) {
+                nums1[len--]=nums1[l1--];
+                //Checking value in nums1 greater than nums2
+            }else{
+                nums1[len--]=nums2[l2--];
+                //Vice versa of checking
+            }
+        }        
     }
 }
