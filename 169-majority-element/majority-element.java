fc3798ca775a36d5796e08f34388a184cc397875
suriya216 <136613620+suriya216@users.noreply.github.com>
2024-03-25 21:59:52 +0530

    Time: 13 ms (27.45%) | Memory: 49.2 MB (76.21%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -3,10 +3,12 @@ class Solution {
         Map<Integer, Integer> m=new HashMap<>();
         for(int i=0;i<nums.length;i++){
             m.put(nums[i], m.getOrDefault(nums[i], 0)+1);
+            //Finding occurences of each values in given array
         }
         int n=nums.length/2;
         for(Map.Entry<Integer, Integer> e:m.entrySet()){
             if(e.getValue()>n)
+            //Checking the occurence of the value greater the n/2
                 return e.getKey();
         }
         return 0;
