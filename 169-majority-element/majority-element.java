dbe145ee72412acbc2e53ad1753c0ed91d80001c
suriya216 <136613620+suriya216@users.noreply.github.com>
2024-03-25 21:58:07 +0530

    Time: 13 ms (27.45%) | Memory: 49.4 MB (72.75%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,7 +1,14 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);  //Sort the array in ascending order
-        int x=nums.length/2;    //Finding the middle index
-        return nums[x]; //As per the concept number mus be occure n/2 times it will definitely takes place in middle index
+        Map<Integer, Integer> m=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            m.put(nums[i], m.getOrDefault(nums[i], 0)+1);
+        }
+        int n=nums.length/2;
+        for(Map.Entry<Integer, Integer> e:m.entrySet()){
+            if(e.getValue()>n)
+                return e.getKey();
+        }
+        return 0;
     }
 }
