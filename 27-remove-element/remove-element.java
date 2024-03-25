259d30146021045899fc9a05c187a4c180871680
suriya216 <136613620+suriya216@users.noreply.github.com>
2024-03-25 21:10:48 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (62.39%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,12 +1,13 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int index = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                nums[index] = nums[i];
+        int index=0;//Temporary value for storing index of the array element
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=val){
+                //Comparing each value which is not equals to the "val"
+                nums[index]=nums[i];
                 index++;
             }
         }
-        return index;
+        return index;//Returing recently pointing index
     }
 }
