33f73748a9c70de40ff87893f819ca8c1dc4428b
suriya216 <136613620+suriya216@users.noreply.github.com>
2024-03-29 14:04:42 +0530

    Time: 3 ms (56.47%) | Memory: 45.7 MB (42.19%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,10 +1,12 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int reachable = 0;
+        int reachable = 0;  //Temporary variable holding upcoming place
         for(int i = 0; i < nums.length; i ++) {
            if(i > reachable) return false;
+           //if current position is greater than the previously stored value then return false
            reachable = Math.max(reachable, i + nums[i]);
         } 
+        //Unless less than or equal to the previous value, it returns true
        return true;
     }
 }
