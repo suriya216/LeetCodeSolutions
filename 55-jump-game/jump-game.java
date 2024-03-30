a101dbd23b691b54bf3522e2d4df4bb2c22bc4a8
suriya216 <136613620+suriya216@users.noreply.github.com>
2024-03-30 10:24:20 +0530

    Time: 1 ms (99.95%) | Memory: 45.8 MB (31.09%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,11 +1,15 @@
 class Solution {
     public boolean canJump(int[] nums) {
+        //Traversing the array by reverse order to achieve the goal
         int lastPos=nums.length-1;
         for(int i=nums.length-1;i>=0;i--){
+            //Checking current position + current index greater than or equal to last position index
             if(i+nums[i]>=lastPos){
                 lastPos=i;
             }
+            //Unless last position remains the same
         }
+        //return true if last position reaches 0-th index, otherwise false
         return lastPos==0;
     }
 }
