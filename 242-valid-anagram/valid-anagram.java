7bb837b39f39777e00b82fd7c4611977d82507ed
suriya216 <136613620+suriya216@users.noreply.github.com>
2024-05-23 09:49:08 +0530

    Time: 2 ms (97.73%) | Memory: 42.9 MB (82.37%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int []count=new int[26];
+        for(char c:s.toCharArray()){
+            count[c-'a']++;
+        }
+        for(char c:t.toCharArray()){
+            count[c-'a']--;
+        }
+        for(int val:count){
+            if(val!=0){
+                return false;
+            }
+        }
+        return true;
+        // Map<Character, Integer> m=new HashMap<>();
+        // for(char c:s.toCharArray()){
+        //     m.put(c, m.getOrDefault(c, 0)+1);
+        // }
+        // for(char c:s.toCharArray()){
+        //     m.put(c, m.getOrDefault(c, 0)-1);
+        // }
+        // for(int res:m.values()){
+        //     if(res!=0){
+        //         return false;
+        //     }
+        // }
+        // return true;
+    }
+}
