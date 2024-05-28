9bc94a8fef1ff8504174fb64e2718583a7ea26b9
suriya216 <136613620+suriya216@users.noreply.github.com>
2024-05-28 09:43:17 +0530

    Time: 18 ms (49.91%) | Memory: 45 MB (48.08%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,29 +1,38 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (s==null||t==null||s.length()==0||t.length()==0||s.length()<t.length()) {
-            return new String();
+        if (t.length() > s.length()) return "";
+        Map<Character, Integer> charCountMap = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            charCountMap.put(c, charCountMap.getOrDefault(c, 0) + 1);
         }
-        int[] map= new int[128];
-        int count=t.length();
-        int start=0, end=0, minLen=Integer.MAX_VALUE, startIndex=0;
-        for (char c:t.toCharArray()) {
-            map[c]++;
-        }
-        char[] chS=s.toCharArray();
-        while (end<chS.length) {
-            if (map[chS[end++]]-->0) {
-                count--;
-            }
-            while (count==0) {
-                if (end-start<minLen) {
-                    startIndex=start;
-                    minLen=end-start;
-                }
-                if (map[chS[start++]]++==0) {
-                    count++;
+        int matchCounter = charCountMap.size();
+        int left = 0, right = 0;
+        int minLen = Integer.MAX_VALUE;
+        int minStart = 0;
+        while (right < s.length()) {
+            char rightChar = s.charAt(right);
+            if (charCountMap.containsKey(rightChar)) {
+                charCountMap.put(rightChar, charCountMap.get(rightChar) - 1);
+                if (charCountMap.get(rightChar) == 0) {
+                    matchCounter--;
                 }
             }
+            right++;
+            while (matchCounter == 0) {
+                if (right - left < minLen) {
+                    minLen = right - left;
+                    minStart = left;
+                }
+                char leftChar = s.charAt(left);
+                if (charCountMap.containsKey(leftChar)) {
+                    charCountMap.put(leftChar, charCountMap.get(leftChar) + 1);
+                    if (charCountMap.get(leftChar) > 0) {
+                        matchCounter++;
+                    }
+                }
+                left++;
+            }
         }
-        return minLen==Integer.MAX_VALUE?new String():new String(chS, startIndex, minLen);
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(minStart, minStart + minLen);
     }
 }
