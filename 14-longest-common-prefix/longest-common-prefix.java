fd5c7dd40edad53a0b4e4596cf281277db703a1d
suriya216 <136613620+suriya216@users.noreply.github.com>
2024-05-29 09:19:22 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (82.46%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -4,7 +4,6 @@ class Solution {
         for(int i=1;i<strs.length;i++){
             while(strs[i].indexOf(prefix)!=0){
                 prefix=prefix.substring(0,prefix.length()-1);
-                System.out.println(prefix);
                 if(prefix.isEmpty()){
                     return "";
                 }
