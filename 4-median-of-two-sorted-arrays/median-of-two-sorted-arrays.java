f9bfb4106712f6641e20c0860b7c7a8c7794a9d1
suriya216 <136613620+suriya216@users.noreply.github.com>
2024-05-31 09:53:42 +0530

    Time: 1 ms (100.00%) | Memory: 45.7 MB (71.93%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,19 +1,38 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1=nums1.length;
-        int n2=nums2.length;
-        int []arr = new int[n1+n2];
-        int i=0,j=0;
-        for(i=0;i<n1;i++){
-            arr[j++]=nums1[i];
+        if (nums1.length > nums2.length) {
+            int[] temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
         }
-        for(i=0;i<n2;i++){
-            arr[j++]=nums2[i];
+
+        int x = nums1.length;
+        int y = nums2.length;
+        int low = 0, high = x;
+
+        while (low <= high) {
+            int partitionX = (low + high) / 2;
+            int partitionY = (x + y + 1) / 2 - partitionX;
+
+            int maxX = (partitionX == 0) ? Integer.MIN_VALUE : nums1[partitionX - 1];
+            int minX = (partitionX == x) ? Integer.MAX_VALUE : nums1[partitionX];
+
+            int maxY = (partitionY == 0) ? Integer.MIN_VALUE : nums2[partitionY - 1];
+            int minY = (partitionY == y) ? Integer.MAX_VALUE : nums2[partitionY];
+
+            if (maxX <= minY && maxY <= minX) {
+                if ((x + y) % 2 == 0) {
+                    return (double) (Math.max(maxX, maxY) + Math.min(minX, minY)) / 2.0;
+                } else {
+                    return (double) Math.max(maxX, maxY);
+                }
+            } else if (maxX > minY) {
+                high = partitionX - 1;
+            } else {
+                low = partitionX + 1;
+            }
         }
-        Arrays.sort(arr);
-        if((n1+n2)%2 != 0){
-            return (double)arr[(n1+n2)/2];
-        }
-        return (double)((arr[(arr.length/2)-1])+(arr[arr.length/2]))/2.0;
+
+        throw new IllegalArgumentException("Input arrays are not sorted!");
     }
 }
